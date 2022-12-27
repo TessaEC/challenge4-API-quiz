58a589330ac7ce17428bb0e0bb822ac60066bf1c
TessaEC <118077000+TessaEC@users.noreply.github.com>
2022-12-27 08:53:45 -0600

    All pseudo code, added htmls

---
+++ script.js
@@ -0,0 +1,31 @@
+// set Const
+
+// Start button
+//var generateBtn = document.querySelector("#generateBtn");
+
+// Event listener for Start button
+// var start
+//generateBtn.addEventListener("click", );
+
+// timer and questions start - how long?
+// 60 seconds- countdown
+// Questions with clickable options
+
+// view high scores button on each page
+
+// when questions answered - next question
+// 5 questions ok?
+
+// incorrect answer - subtract time from clock - how much?
+// subtract 5 seconds for wrong answer?
+
+// set Correct or wrong for each answer
+
+// all questions answered OR time hits zero - display game is over
+
+// All done! when all answered or Game over if run out of time
+
+// display score - how to calculate?
+
+// then save initials and score
+//create submit button - only clickable when something typed in box
