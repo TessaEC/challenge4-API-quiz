f4036c568ee83d3f932890f8c73d854000b2a92e
TessaEC <118077000+TessaEC@users.noreply.github.com>
2023-01-04 10:56:54 -0600

    Updated html, added css and started JS code

--- script.js
+++ script.js
@@ -1,31 +1,135 @@
-// set Const
+// variables
+// var timer;
+var timeStart = questions.length * 1;
+var questIndex = 0;
+var timeInterval;
+// var timed;
+// HTML DOM elements
+var startPage = document.getElementById('start-page');
+var startBtn = document.getElementById('start');
+var questionsPage = document.getElementById('questions');
+var timerId = document.getElementById('time');
+var submitBtn = document.getElementById('submit');
+var scoresId = document.getElementById('scores');
+var finishId = document.getElementById('finish');
+var optionsId = document.getElementById('optionArr');
+var optionBtn = document.getElementById('options')
 
-// Start button
-//var generateBtn = document.querySelector("#generateBtn");
+// Start button event listener
+startBtn.addEventListener('click', function() {
+    startPage.classList.add("hidden");
+    questionsPage.classList.remove("hidden");
 
-// Event listener for Start button
-// var start
-//generateBtn.addEventListener("click", );
+// Start timer
+    timeInterval = setInterval(clockTick, 1000);
 
-// timer and questions start - how long?
-// 60 seconds- countdown
-// Questions with clickable options
+    getQuestion();
+})
 
-// view high scores button on each page
+// set function for intervals
+function clockTick() {
+    if (timeStart >= 1) {
+        // Set textContent to show timeStart
+        timerId.textContent = timeStart;
+        // Decrease timeStart by 1
+        timeStart--;
+      } else {
+        // when timeStart gets to 0, set timerId to an empty string
+        timerId.textContent = '';
+        // clearInterval is used to stop the timer
+        clearInterval(timeInterval);
+        // show final-score page when timer hits 0 or questions end
+      }
+    if (timeStart < 1) {
+        questionsPage.classList.add("hidden")
+        finishId.classList.remove("hidden")
+      }   
+}
 
-// when questions answered - next question
-// 5 questions ok?
+// function for displaying questions
+function getQuestion() {
+    var currentQuest = questions[questIndex]
+    var questionDisplay = document.getElementById('question-title')
+    questionDisplay.textContent=currentQuest.title
 
-// incorrect answer - subtract time from clock - how much?
-// subtract 5 seconds for wrong answer?
+    // document.getElementById("options-0")
+    // document.getElementById("options-0").textContent = words
+    // loop through question options and update each span
+    // loop questions + options button
+// loop currentQuestion
+// function loopAtt() {
+//     for (var i = 0; i < questIndex.questions.length; i++) {
+//         var quiz = questIndex.questions[i];
+//         var optBtn = document.createElement('optionArr')
+//         optBtn.setAttribute('class', 'options');
+//         optBtn.setAttribute('value', options);
+    
+//         quiz.textContent = i + 1 + questions;
+//     }
+}
 
-// set Correct or wrong for each answer
+// Event listener and function for option clicks
+// function getOptions() {
+//     var optionDisplay = document.getElementById('optionArr')
+//     optionDisplay.textContent=options[0].options
+// view your options inside of the buttons
+// document.getElementById("options-0")
+// document.getElementById("options-0").textContent = 'words'
+// }
+    
 
-// all questions answered OR time hits zero - display game is over
 
-// All done! when all answered or Game over if run out of time
+// Event Listener:
+// select correct answer-display correct! -move to next question
+// select wrong answer-display wrong! -subtract 5 seconds from time - move to next question
 
-// display score - how to calculate?
+// function for ending quiz
+    //time runs out or finished with questions
 
-// then save initials and score
-//create submit button - only clickable when something typed in box
+/* finished:
+
+
+
+finish page
+    display score
+    paragraph stating Time ran out OR Quiz Finished
+    'Submit Name' with text box
+    Submit button
+
+submitting high scores on Leader board
+    printScores(); ?
+    sort highest score to lowest
+
+storing scores once submitted to leader board
+
+***HighScores button on top left, can click anytime to view leader board
+
+
+NOTES
+
+JS -
+startBtn.addEvenListener("onClick", startQuiz)]
+
+function to remove start screen once next screen starts
+-unhide -remove attribute
+-hide
+QUIZ:
+start quiz: fip through the questions
+-var currentQuestionIndex start at 0 index
+Grab title element from html then set to current question
+keep track of what the user chose
+For loop for options]
+Add listeners to each choice, will display if correct or wrong once clicked, if correct
+states correct and moves to next button
+event.target grabs the answer that was clicked
+if the click element was not a choice element do nothing
+return-stops running
+if the button that was clicked value != questions[currentquestionindex].answer) {
+    if its not then penalize time
+    time -= 15
+
+    feedback in html hide Wrong!
+    feedback in html hide Correct!
+    value = time
+}
+*/
