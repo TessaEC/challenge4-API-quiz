6953eba7b09cddd63fe55e76cb1e6605d77c3025
TessaEC <118077000+TessaEC@users.noreply.github.com>
2023-01-04 10:56:54 -0600

    finished html, added css and started JS code

---
+++ quiz.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Which operator should be used when comparing both values and types?",
+        options: ["=", "!=", "||", "==="],
+        answer: 3
+    },
+    {
+        title: "What ocurrs when you want to get the sum of 3 + '3'?",
+        options: ["An Error", "sum = 6", "Coercion to string 33", "A function"],
+        answer: 2
+    },
+    {
+        title: "What function returns a boolean?",
+        options: ["isNan()", "typeOf(undefined)", "var b = 25", "function boolean()"],
+        answer: 0
+    },
+    {
+        title: "What does the 'this' keyword refer to?",
+        options: ["callback function", "output of console.log", "object", "return"],
+        answer: 2
+    },
+    {
+        title: "What is an example of using a prototype to get access to an object?",
+        options: ["var arr = []", "arr.push(2)", "console.log(arr)", "function arr{}"],
+        answer: 1
+    },
+]
